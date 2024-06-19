ea48e7d311bdf78351f753b46e73b4a38bcb43ea
Ethan Lin <eclin3@uci.edu>
2024-06-18 22:49:37 -0700

    Added parser and insert into db

---
+++ drug_parser.py
@@ -0,0 +1,50 @@
+import requests
+import mysql.connector
+from bs4 import BeautifulSoup
+from mysql_constants import Constants
+
+page_url = 'https://www.webmd.com/drugs/2/index'
+
+def parse_webpage(url):
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+    }
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 200:
+        page_content = BeautifulSoup(response.content, 'html.parser')
+
+        all_drugs = page_content.find_all('li', class_ = 'list-items')
+        if all_drugs:
+            for drug in all_drugs:
+                drug_name = drug.find('a', class_='common-drug-name')
+                if drug_name:
+                    insert_SQL(drug_name.get_text())
+
+        else:
+            print("error")
+
+        print("Inserted all medicines")
+
+    else:
+        print(f"Failed to retrieve the webpage. Status code: {response.status_code}")
+
+
+def insert_SQL(drug_name):
+    try:
+        connection = mysql.connector.connect(user=Constants.USER, password=Constants.PASSWORD, database=Constants.DATABASE)
+        cursor = connection.cursor(buffered=True)
+
+        insert_statement = """INSERT INTO medicines (medicine_name) VALUES (%s);"""
+
+        cursor.execute(insert_statement, (drug_name,))
+
+    except mysql.connector.Error as error:
+        print("Medicine already exists")
+
+    finally:
+        connection.commit()
+        cursor.close()
+        connection.close()
+
+parse_webpage(page_url)
---
+++ mysql_constants.py
@@ -0,0 +1,5 @@
+class Constants:
+    USER = "root"
+    PASSWORD = "2003"
+    HOST = "localhost"
+    DATABASE = "medicinetracker"
